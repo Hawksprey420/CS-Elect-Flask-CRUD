b87ecf5237c97192344b5685b9427a09d2920f90
Hawksprey420 <nestorjannasag@gmail.com>
2025-12-14 16:50:28 +0800

    added config files

--- config/config.py
+++ config/config.py
@@ -4,7 +4,7 @@ class SystemConfig:
     MYSQL_HOST = os.environ.get('MYSQL_HOST', 'localhost')
     MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
     MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', 'root123')
-    MYSQL_DB = os.environ.get('MYSQL_DB', 'flask_crud_db')
+    MYSQL_DB = os.environ.get('MYSQL_DB', 'ucms_enrollment') # replace this with actual later
     MYSQL_PORT = int(os.environ.get('MYSQL_PORT', 3306))
     MYSQL_POOL_NAME = os.environ.get('MYSQL_POOL_NAME', 'flask_pool')
     MYSQL_POOL_SIZE = int(os.environ.get('MYSQL_POOL_SIZE', 5))
