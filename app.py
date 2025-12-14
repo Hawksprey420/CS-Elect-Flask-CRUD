bf3cf4813d594e67174d683bdd583c9dcd888037
Hawksprey420 <nestorjannasag@gmail.com>
2025-12-14 17:01:09 +0800

    Initial response format for endpoints and initial JWT token implementation

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_mysqldb import MySQL
 from config.config import SystemConfig
 import jwt
 import datetime
+import dicttoxml
 from functools import wraps
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = SystemConfig.MYSQL_HOST
@@ -11,3 +12,45 @@ app.config['MYSQL_HOST'] = SystemConfig.MYSQL_HOST
 mysql = MySQL(app)
 app.config.from_object(SystemConfig)
 
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.headers.get('Authorization')
+        if not token:
+            return jsonify({'message': 'Token is missing!'}), 401
+        try:
+            if token.startswith('Bearer '):
+                token = token.split(" ")[1]
+            data = jwt.decode(token, app.config['JWT_SECRET_KEY'], algorithms=["HS256"])
+        except:
+            return jsonify({'message': 'Token is invalid!'}), 401
+        return f(*args, **kwargs)
+    return decorated
+
+def format_response(data, status_code=200):
+    fmt = request.args.get('format', 'json')
+    if fmt == 'xml':
+        xml = dicttoxml.dicttoxml(data, custom_root='response', attr_type=False)
+        response = make_response(xml)
+        response.headers['Content-Type'] = 'application/xml'
+    else:
+        response = make_response(jsonify(data))
+        response.headers['Content-Type'] = 'application/json'
+    
+    response.status_code = status_code
+    return response
+
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+        if not token:
+            return jsonify({'message': 'Token is missing!'}), 401
+        try:
+            data = jwt.decode(token, app.config['JWT_SECRET_KEY'], algorithms=["HS256"])
+            current_user = data['user']
+        except:
+            return jsonify({'message': 'Token is invalid!'}), 401
+        return f(current_user, *args, **kwargs)
+    return decorated
